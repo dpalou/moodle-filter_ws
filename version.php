365c0f62c82c5f90c314e95a3bbdf1a55e5511d4
Dani Palou <dani@moodle.com>
2024-11-25 13:03:46 +0100

    Bump version to 1.0.2

--- version.php
+++ version.php
@@ -24,8 +24,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2024100801;          // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2024100802;          // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2016052300;          // Requires this Moodle version.
 $plugin->component = 'filter_ws';         // Full name of the plugin (used for diagnostics).
-$plugin->release   = '1.0.1';
+$plugin->release   = '1.0.2';
 $plugin->maturity  = MATURITY_STABLE;
